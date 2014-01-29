15ad3db55318ed10b40f22b1aa3013be9a77e9c4
Jaime Iniesta <jaimeiniesta@gmail.com>
2014-01-29 20:29:32 +0100

    improves the light line follower by making it turn using opposite directions in wheels.

--- redbot_light_line_follower/redbot_light_line_follower.ino
+++ redbot_light_line_follower/redbot_light_line_follower.ino
@@ -1,15 +1,25 @@
 /************************************************************************
- RedBot light follower using line sensors
- ----------------------------------------
- 
- This is a simple script to make a RedBot follow a light projected on the
- floor. It uses line sensors, which are not really thought for following
- light, but for following lines, but that's what I had for a start :)
- 
- Just make sure you place your sensors on the front and quite apart one
- from the other.
-
-************************************************************************/
+ * RedBot light follower using line sensors
+ * ----------------------------------------
+ * 
+ * This is a simple script to make a RedBot follow a light projected on the
+ * floor. It uses line sensors, which are not really thought for following
+ * light, but for following lines, but that's what I had for a start :)
+ * 
+ * Just make sure you place your sensors on the front and quite apart one
+ * from the other.
+ * 
+ * Some lessons learned building this:
+ *
+ *   - to turn, rather than braking a wheel, make them run in opposite
+ *     directions. This way it can recover from crashing into walls
+ *
+ *   - running at a moderate speed makes it easier to turn due to
+ *     less inertia. Well, as in real life :)
+ *
+ *   - you don't need a delay inside the loop
+ * 
+ ************************************************************************/
 
 #include <RedBot.h>
 #include <SoftwareSerial.h>
@@ -19,6 +29,8 @@ RedBotMotor motor;
 RedBotSensor lSen = RedBotSensor(2);
 RedBotSensor cSen = RedBotSensor(3);
 RedBotSensor rSen = RedBotSensor(6);
+
+int mainSpeed = 150;
 
 void setup() {
   Serial.begin(57600);
@@ -35,8 +47,6 @@ void loop() {
   else {
     goRight();
   }
-
-  delay(100);
 }
 
 int lightDifference() {
@@ -45,19 +55,19 @@ int lightDifference() {
 
 void goStraight() {
   Serial.println("STRAIGHT");
-  motor.drive(100);
+  motor.drive(mainSpeed);
 }
 
 void goLeft() {
   Serial.println("LEFT!!!");
-  motor.rightDrive(255);
-  motor.leftStop();
+  motor.rightDrive(mainSpeed);
+  motor.leftDrive(-mainSpeed);
 }
 
 void goRight() {
   Serial.println("RIGHT!!!");
-  motor.rightStop();
-  motor.leftDrive(255);
+  motor.rightDrive(-mainSpeed);
+  motor.leftDrive(mainSpeed);
 }
 
 
