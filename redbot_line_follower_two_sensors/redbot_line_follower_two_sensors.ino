9858291cbf7ce7076245d63e201f0afcf82c2d30
Jaime Iniesta <jaimeiniesta@gmail.com>
2014-02-04 19:01:04 +0100

    adds a script for following a line using two sensors

---
+++ redbot_line_follower_two_sensors/redbot_line_follower_two_sensors.ino
@@ -0,0 +1,62 @@
+/************************************************************************
+ * RedBot line follower using two sensors
+ * ----------------------------------------
+ * 
+ * This is a simple script to make a RedBot follow a dark line 
+ * using two line sensors
+ *
+ * You should set the lineContrast depending on the perceived contrast
+ * between the line and the surface.
+ * 
+ ************************************************************************/
+
+#include <RedBot.h>
+#include <SoftwareSerial.h>
+
+RedBotMotor motor;
+
+RedBotSensor lSen = RedBotSensor(2);
+RedBotSensor rSen = RedBotSensor(6);
+
+const int mainSpeed    = 200;
+const int lineContrast = 300;
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println("Hello world!");
+}
+
+void loop() {  
+  if (detectedContrast() < lineContrast) {
+    goStraight();
+  } 
+  else if (lSen.read() > rSen.read()) {
+    turnLeft();
+  } 
+  else {
+    turnRight();
+  }
+}
+
+int detectedContrast() {
+  return abs(lSen.read() - rSen.read());
+}
+
+void goStraight() {
+  Serial.println("STRAIGHT");
+  motor.drive(mainSpeed);
+}
+
+void turnLeft() {
+  Serial.println("LEFT!!!");
+  motor.leftStop();
+  motor.rightDrive(mainSpeed);
+}
+
+void turnRight() {
+  Serial.println("RIGHT!!!");
+  motor.rightStop();
+  motor.leftDrive(mainSpeed);
+}
+
+
